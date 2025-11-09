360c9e3228ac5ee869f02e7d89c050e7b0bb00a5
Alí Sinisterra <haidarconh@gmail.com>
2025-11-09 15:19:10 -0500

    use .paragraph-block in main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,17 @@
 <section id="about-me">
     <h1>Hello!</h1>
-    <p>
-        You can call me Ali. I have been here on Earth for some 5,961 days 
-        (or 16 years). I was born in Cali, VAC, Colombia, and currently live 
-        in a small town nearby.
-    </p>
-    <p>
-        Making things makes me happy. Although I’m not particularly good at 
-        anything, I enjoy words, design, and fiddling with computers.
-    </p>
-    <p>
-        Hack Club is cool. You should check it out!
-    </p>
+    <div class="paragraph-block">
+        <p>
+            You can call me Ali. I have been here on Earth for some 5,961 days 
+            (or 16 years). I was born in Cali, VAC, Colombia, and currently live 
+            in a small town nearby.
+        </p>
+        <p>
+            Making things makes me happy. Although I’m not particularly good at 
+            anything, I enjoy words, design, and fiddling with computers.
+        </p>
+        <p>
+            Hack Club is cool. You should check it out!
+        </p>
+    </div>
 </section>
