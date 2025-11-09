71a4bc2e2625222e068f7d97143084be6cce1585
Alí Sinisterra <haidarconh@gmail.com>
2025-11-09 16:03:22 -0500

    make times in about me dynamic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
+<script lang="ts">
+    import moment from "moment";
+
+    let birthDate = moment([2009, 7, 15])
+</script>
+
 <section id="about-me">
     <h1>Hello!</h1>
     <div class="paragraph-block">
         <p>
-            You can call me Ali. I have been here on Earth for some 5,961 days 
-            (or 16 years). I was born in Cali, VAC, Colombia, and currently live 
-            in a small town nearby.
+            You can call me Ali. I have been here on Earth for some 
+            {moment().diff(birthDate, "days")} days (or 
+            {moment().diff(birthDate, "years")} years). I was born in Cali, 
+            VAC, Colombia, and currently live in a small town nearby.
         </p>
         <p>
             Making things makes me happy. Although I’m not particularly good at 
