6115f4cebd38eae688510cde2094b5903d01946e
Alí Sinisterra Rojas <haidarconh@gmail.com>
2025-11-10 18:46:56 -0500

    ...oops, edted the wrong file :clown:

--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
 	import { page } from '$app/state';
+	import AnchorGrid from '$lib/components/AnchorGrid.svelte';
 
-	type PicturePost = {
+	type WritingPost = {
 		date: string,
-		href: string
+		path: string,
+		title: string
 	}
 
 	let posts = $derived(
 		(page.data.posts || []).map((post: any) => ({
-			date: post.meta.title,
-			href: post.path
-		} as PicturePost))
+			title: post.meta.title,
+			date: post.meta.date,
+			path: post.path
+		} as WritingPost))
 	);
 </script>
 <section class="primary">
-    <h1>Pictures</h1>
+    <h1>Writing</h1>
+	<section>
+		<h2>Poetry</h2>
+		<AnchorGrid items={posts} />
+	</section>
 </section>
 
