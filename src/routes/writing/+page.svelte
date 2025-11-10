3a603ca8d3cb153a274ebffc979879c87632865f
Alí Sinisterra <haidarconh@gmail.com>
2025-11-09 19:35:07 -0500

    use markdown files yay

--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -1,6 +1,11 @@
-<script>
+<script lang="ts">
 	import AnchorGrid from "$lib/components/AnchorGrid.svelte";
+	import { page } from '$app/state';
 
+	$: posts = (page.data.posts || []).map((post) => ({
+		name: post.meta.title,
+		anchor: post.path
+	}));
 </script>
 <section class="primary">
     <h1>Writing</h1>
@@ -8,23 +13,6 @@
         <h2>Poetry</h2>
     </section>
 
-    <AnchorGrid items={[
-        {
-            "anchor": "https://google.com",
-            "name": "Good things wither when they grace my hands"
-        },
-        {
-            "anchor": "https://google.com",
-            "name": "Google"
-        },
-        {
-            "anchor": "https://google.com",
-            "name": "Google"
-        },
-        {
-            "anchor": "https://google.com",
-            "name": "Google"
-        },
-    ]}></AnchorGrid>
+    <AnchorGrid items={posts}></AnchorGrid>
 </section>
 
